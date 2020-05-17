3470e1440024d49e593fc33f5c17cf9dc3ea4b49
gokul raj <gokul.raj@gigamon.com>
2020-05-17 19:01:46 +0530

    Fixed the url issue after deployment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
   return(
@@ -8,23 +9,23 @@ const Header = (props) => {
           <a className="navbar-brand" href="/">{props.title}</a>
         </div>
         <ul className="nav navbar-nav navbar-right">
-          <li className="active"><a href="/Agricart-online-vegetable-shopping-reactjs/products"> <i className="glyphicon glyphicon-search icon"></i>Products</a></li>
-          <li ><a href="/Agricart-online-vegetable-shopping-reactjs/offers"><i className="fa fa-percent icon"></i>Offers</a></li>
-          <li><a href="/Agricart-online-vegetable-shopping-reactjs/help"><i className="fa fa-life-ring icon"></i> Help</a></li>
+          <li className="active"><Link to="/Agricart-online-vegetable-shopping-reactjs/products"> <i className="glyphicon glyphicon-search icon"></i>Products</Link></li>
+          <li ><Link to="/Agricart-online-vegetable-shopping-reactjs/offers"><i className="fa fa-percent icon"></i>Offers</Link></li>
+          <li><Link to="/Agricart-online-vegetable-shopping-reactjs/help"><i className="fa fa-life-ring icon"></i> Help</Link></li>
           
           {props.user.userId!=null?(<li className="dropdown">
             <a href="#" className="dropdown-toggle" data-toggle="dropdown"><span className="glyphicon glyphicon-user icon"></span>{props.user.email!==null? props.user.email:"Hello, Sign in"}</a>
             <ul className="dropdown-menu">
               <li align="center" className="well">
-              {props.user.email===null?(<a href="/Agricart-online-vegetable-shopping-reactjs/signin"><span className="glyphicon glyphicon-user icon"></span>Sign In</a>):''}
-                  <a href="/Agricart-online-vegetable-shopping-reactjs/profile" > Profile</a>
-                  <a href="/Agricart-online-vegetable-shopping-reactjs/orders" > Orders</a>
-                  <a href="/Agricart-online-vegetable-shopping-reactjs/favourites" > Favourites</a>
+              {props.user.email===null?(<Link to="/Agricart-online-vegetable-shopping-reactjs/signin"><span className="glyphicon glyphicon-user icon"></span>Sign In</Link>):''}
+                  <Link to="/Agricart-online-vegetable-shopping-reactjs/profile" > Profile</Link>
+                  <Link to="/Agricart-online-vegetable-shopping-reactjs/orders" > Orders</Link>
+                  <Link to="/Agricart-online-vegetable-shopping-reactjs/favourites" > Favourites</Link>
                   {props.user.email!==null?(<a onClick={props.logout} ><span className="glyphicon glyphicon-log-out"></span> Logout</a>):''}
               </li>
               </ul>
             </li>):''}
-          <li><a href="/checkout"><i className="fa fa-cart-plus icon"></i>Cart <span>{props.count.length}</span> </a></li>
+          <li><Link to="/checkout"><i className="fa fa-cart-plus icon"></i>Cart <span>{props.count.length}</span> </Link></li>
         </ul>
       </div>
     </nav>
