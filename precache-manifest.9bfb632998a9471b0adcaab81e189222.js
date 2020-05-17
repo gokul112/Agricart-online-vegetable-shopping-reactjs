52d40b997421a4c31877156386840c4cc19c5d61
gokul raj <gokul.raj@gigamon.com>
2020-05-17 19:19:29 +0530

    Updates

--- precache-manifest.9bfb632998a9471b0adcaab81e189222.js
+++ precache-manifest.9bfb632998a9471b0adcaab81e189222.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d3f123fe41dc89da5a2f88775ac09a5d",
+    "revision": "774b83b82783b3cb2edd266ab3127904",
     "url": "/Agricart-online-vegetable-shopping-reactjs/index.html"
   },
   {
-    "revision": "f6a1c8deff5fc20deaf1",
+    "revision": "c69cd319def064866e92",
     "url": "/Agricart-online-vegetable-shopping-reactjs/static/css/main.89c165c6.chunk.css"
   },
   {
-    "revision": "4ee02f124d96e1794efc",
-    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/2.26bef023.chunk.js"
+    "revision": "ac0fd68b2a9abd71f312",
+    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/2.037ad77c.chunk.js"
   },
   {
     "revision": "0f59aded427042d87c15d0910a90fb3b",
-    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/2.26bef023.chunk.js.LICENSE.txt"
+    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/2.037ad77c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f6a1c8deff5fc20deaf1",
-    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/main.46e85b6d.chunk.js"
+    "revision": "c69cd319def064866e92",
+    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/main.78c35318.chunk.js"
   },
   {
     "revision": "38804e9b75dc848b1c88",
