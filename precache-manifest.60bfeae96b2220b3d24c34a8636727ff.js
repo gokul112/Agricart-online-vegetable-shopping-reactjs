ba0270259fb3760a74718bc70e16be7162f70937
gokul raj <gokul.raj@gigamon.com>
2020-05-17 16:36:20 +0530

    Updates

---
+++ precache-manifest.60bfeae96b2220b3d24c34a8636727ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3d1a14c2dadecedacde11055ff6ee3",
+    "url": "/Agricart-online-vegetable-shopping-reactjs/index.html"
+  },
+  {
+    "revision": "de4ca3f1b27546d704a4",
+    "url": "/Agricart-online-vegetable-shopping-reactjs/static/css/main.89c165c6.chunk.css"
+  },
+  {
+    "revision": "ac0fd68b2a9abd71f312",
+    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/2.037ad77c.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/2.037ad77c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4ca3f1b27546d704a4",
+    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/main.f42313ed.chunk.js"
+  },
+  {
+    "revision": "38804e9b75dc848b1c88",
+    "url": "/Agricart-online-vegetable-shopping-reactjs/static/js/runtime-main.6c4047ee.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Agricart-online-vegetable-shopping-reactjs/precache-manifest.60bfeae96b2220b3d24c34a8636727ff.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Agricart-online-vegetable-shopping-reactjs/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
